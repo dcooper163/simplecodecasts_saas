79ef975b6289f0be7ce9a0a55e37bbea86c79a68
dcooper163 <dcooper163@gmail.com>
2016-09-06 21:06:02 +0000

    basic and pro params added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
     def home
-        @basic_plan = Plan.find(1)
-        @pro_plan = Plan.find(2)
+        @basic_plan = Plan.find_by_name('basic')
+        @pro_plan = Plan.find_by_name('pro')
     end
     
     def about
