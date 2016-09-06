12b562d013d3b5716f368ea7299aa283cea6f6a4
dcooper163 <dcooper163@gmail.com>
2016-09-06 09:27:01 +0000

    Changed production variable

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-  default to:  'dcooper163@gmail.com'
+  default to: 'dcooper163@gmail.com'
     
     def contact_email(name, email, body)    
     @name = name
