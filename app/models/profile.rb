b92c6e22a96ff5d4e6927fb98602d98853c80008
dcooper163 <dcooper163@gmail.com>
2016-11-26 08:25:49 +0000

    Made improvements to user profile presentation

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,7 +3,7 @@ class Profile < ActiveRecord::Base
     belongs_to :user
     
     #quick start o using paperclip gem - image uploader - https://github.com/thoughtbot/paperclip
-    has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+    has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/assets/anonymous_avatar.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
     
 end
